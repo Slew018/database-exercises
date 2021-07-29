9328c2f33a68d8eacc5187c0533311a6c79e1c12
Sean Lewis <sean.lewis018@gmail.com>
2021-07-29 09:15:14 -0500

    where clause sql

---
+++ where_exercises.sql
@@ -0,0 +1,38 @@
+USE employees;
+
+SELECT first_name
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT last_name
+FROM  employees
+WHERE last_name LIKE ('E%');
+
+SELECT count(*)
+FROM employees
+WHERE last_name LIKE '%q%';
+
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+SELECT *
+FROM employees.employees
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND gender = 'm';
+
+SELECT *
+FROM employees.employees
+WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
+
+SELECT *
+FROM employees.employees
+WHERE last_name LIKE 'E%'
+  AND last_name LIKE '%E';
+
+SELECT count(*)
+FROM employees.employees
+WHERE last_name LIKE '%q%'
+  AND last_name NOT LIKE '%qu%';
